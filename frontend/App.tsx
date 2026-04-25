aaa2b79955992a85b9429ef8351e97b8c29da0bb
Syed-Khizerr <khizersyedworkspace@gmail.com>
2026-04-25 17:40:32 +0530

    Added Home, Workout, Profile screens, with working navigation.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,30 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {HomeScreen}  from './screens/HomeScreen';
+import {ProfileScreen} from './screens/ProfileScreen';
+import {WorkoutSession} from './screens/WorkoutSession';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 import './global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+const Stack = createNativeStackNavigator();
+
+const StackNavigator =() => (
+  <NavigationContainer >
+
+    <Stack.Navigator screenOptions={{headerShown: false}} >
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Workout' component={WorkoutSession} />
+      <Stack.Screen name='Profile' component={ProfileScreen} />
+    </Stack.Navigator>
+    
+  </NavigationContainer>
+  );
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <ScreenContent title="Work-out Tracker" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <>
+    <StackNavigator/>
+    </>
   );
 }
